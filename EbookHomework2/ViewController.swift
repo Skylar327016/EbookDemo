b3aac83333a9871b234db2f65853b2137ceee2e3
Skylar <apa6143171@gmail.com>
2020-07-26 10:43:10 +0800

    Initial Commit

---
+++ EbookHomework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EbookHomework2
+//
+//  Created by 陳家豪 on 2020/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
